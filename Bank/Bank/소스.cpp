bd8cf92ef3f008b17a12aabc3eef783c70217479
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2020-07-07 17:03:48 +0900

    BANK_STEP_4
    C++ 과제 STEP3 완료!

--- Bank/Bank/소스.cpp
+++ Bank/Bank/소스.cpp
@@ -5,11 +5,12 @@
 using namespace std;
 
 class Account {
-private:
+protected:
 	const int accID;
 	int balance;
-	const int Resident_registration_number;
+	const long long int Resident_registration_number;
 	char* cusName;
+
 public:
 	Account(int accID, int balance, int Resident_registration_number, char* cusName)	//생성자
 		: accID(accID), Resident_registration_number(Resident_registration_number)
@@ -17,6 +18,8 @@ public:
 		this->balance = balance;
 		this->cusName = new char[MAX_LEN];
 		strcpy_s(this->cusName, MAX_LEN, cusName);
+		
+		//이거 다 깊은복사로 만들어야함
 	}
 
 	Account(Account& copy_user)	//복사 생성자
@@ -34,10 +37,17 @@ public:
 	}
 
 	void Deposit(int balance) {
+		this->balance += Interest(balance);
 		this->balance += balance;
 	}
 
-	bool Check_AccID(int accID) const{
+	virtual int Interest(int money) {
+		int interest = (this->balance + money) * 0.01 ;	//이자
+		cout << "이자액 : " << interest << endl << endl;
+		return interest;
+	}
+
+	bool Check_AccID(int accID) const {
 		return this->accID == accID;
 	}
 
@@ -50,15 +60,51 @@ public:
 		return true;
 	}
 
-	void Print_Account_Information() {
+	virtual void Print_Account_Information() {
+		cout << "보통 계좌" << endl;
 		cout << "계좌ID: " << accID << endl;
 		cout << "이  름: " << cusName << endl;
-		cout << "잔  액: " << balance << endl;
-		cout << endl;
+		cout << "잔  액: " << balance << endl << endl;
 	}
 
-	bool Check_SSN(int Resident_registration_number) const{
+	bool Check_SSN(int Resident_registration_number) const {
 		return this->Resident_registration_number == Resident_registration_number;
+	}
+
+	int get_balance()
+	{
+		return balance;
+	}
+
+	const long long int get_Resident_registration_number()
+	{
+		return Resident_registration_number;
+	}
+
+	char* get_cusName()
+	{
+		return cusName;
+	}
+};
+
+class Deposit_Account : public Account {
+public:
+	Deposit_Account(int accID, Account *Member) : Account(accID, 0, Member->get_Resident_registration_number(), Member->get_cusName())
+	{
+		
+	}
+
+	virtual int Interest(int money) {
+		int interest = (this->balance + money) * 0.02;	//이자
+		cout << "이자액 : " << interest << endl << endl;
+		return interest;
+	}
+
+	virtual void Print_Account_Information() {
+		cout << "예금 계좌" << endl;
+		cout << "계좌ID: " << accID << endl;
+		cout << "이  름: " << cusName << endl;
+		cout << "잔  액: " << balance << endl << endl;
 	}
 };
 
@@ -66,6 +112,7 @@ class AccountManager {
 private:
 	Account* Acc_Member[MAX_MEMBER];
 	int Member_Number;
+
 public:
 	AccountManager() {
 		for (int i = 0; i < MAX_MEMBER; i++) {
@@ -75,13 +122,37 @@ public:
 		Member_Number = 0;
 	}
 
+	~AccountManager()
+	{
+		for (int i = 0; i < Member_Number; i++) {
+			delete Acc_Member[i];
+		}
+	}
+
 	void Account_Open() {
+		int account_choice;
+		cout << "개설할 계좌를 선택해 주세요." << endl;
+		cout << "1. 보통 계좌\n2. 예금 계좌" << endl;
+		cin >> account_choice;
+
+		if (account_choice == 1) {
+			Ordinary_Account_Open();
+		}
+		else if (account_choice == 2) {
+			Deposit_Account_Open();
+		}
+		else {
+			cout << "보통 계좌(1)와 예금계좌(2) 중 하나를 선택해주세요." << endl << endl;
+		}
+	}
+
+	void Ordinary_Account_Open() {
 		int accID;
 		int balance;
 		int Resident_registration_number;
 		char cusnName[MAX_LEN];
 
-		if (Member_Number == 100) {
+		if (Member_Number == MAX_MEMBER) {
 			cout << "가입 인원이 초과되었습니다.\n" << endl;
 			return;
 		}
@@ -99,23 +170,64 @@ public:
 		cout << "입금액: ";
 		cin >> balance;
 		if (balance < 10) {
-			cout << "계좌개설 시에는 10원이상의 입금액을 넣어야 합니다.\n" << endl;
+			cout << "계좌개설 시에는 10원이상의 입금액을 넣어야 합니다.\n" << endl << endl;
 			return;
 		}
 
 
 		for (int i = 0; i < Member_Number; i++) {
 			if (Acc_Member[i]->Check_SSN(Resident_registration_number)) {
-				cout << "이미 가입 되었습니다." << endl;
+				cout << "이미 가입 되었습니다." << endl << endl;
 				return;
 			}
 			if (Acc_Member[i]->Check_AccID(accID)) {
-				cout << "이미 있는 계좌ID 입니다." << endl;
+				cout << "이미 있는 계좌ID 입니다." << endl << endl;
 				return;
 			}
 		}
 
 		Acc_Member[Member_Number] = new Account(accID, balance, Resident_registration_number, cusnName);
+
+		Member_Number++;
+		cout << endl;
+	}
+
+	void Deposit_Account_Open() {
+		int accID;
+		int deposit_accID;
+
+		int save_memberNumber = -1;
+
+		cout << "보통 계좌의 계좌번호를 입력해 주세요." << endl;
+		cout << "보통 계좌 ID : ";
+		cin >> accID;
+
+		for (int i = 0; i < Member_Number; i++) {
+			if (Acc_Member[i]->Check_AccID(accID)) {
+				save_memberNumber = i;
+				break;
+			}
+		}
+		if (save_memberNumber == -1)
+		{
+			cout << endl;
+			cout << "보통 계좌 개설 후 예금 계좌를 개설할 수 있습니다." << endl;
+			cout << "보통 계좌를 먼저 만들고 시도하여 주십시오." << endl << endl;
+			return;
+		}
+
+		cout << "예금 계좌에서 사용할 계좌번호를 입력해 주세요." << endl;
+		cout << "예금 계좌 ID : ";
+		cin >> deposit_accID;
+
+		for (int i = 0; i < Member_Number; i++) {
+			if (Acc_Member[i]->Check_AccID(deposit_accID)) {
+				cout << "이미 있는 계좌ID 입니다." << endl << endl;
+				return;
+			}
+		}
+
+		Acc_Member[Member_Number] = new Deposit_Account(deposit_accID, Acc_Member[save_memberNumber]);
 
 		Member_Number++;
 		cout << endl;
@@ -133,12 +245,14 @@ public:
 
 		for (int i = 0; i < Member_Number; i++) {
 			if (Acc_Member[i]->Check_AccID(Check_AccID)) {
+				cout << "a" << endl;
 				Acc_Member[i]->Deposit(Deposit_Amount);
-			}
-			else {
-				cout << "유효하지 않은 ID 입니다." << endl;
+				return;
 			}
 		}
+
+		cout << "유효하지 않은 ID 입니다." << endl << endl;
+	
 		cout << endl;
 	}
 
@@ -155,13 +269,13 @@ public:
 		for (int i = 0; i < Member_Number; i++) {
 			if (Acc_Member[i]->Check_AccID(Check_AccID)) {
 				if (!Acc_Member[i]->Withdraw(Withdrawal_Amount)) {
-					cout << "출금 금액이 현재 잔액보다 많습니다." << endl;
+					cout << "출금 금액이 현재 잔액보다 많습니다." << endl << endl;
 				}
 				return;
 			}
 		}
 
-		cout << "유효하지 않은 ID 입니다." << endl;
+		cout << "유효하지 않은 ID 입니다." << endl << endl;
 		cout << endl;
 	}
 
@@ -174,7 +288,6 @@ public:
 
 
 };
-
 
 
 
